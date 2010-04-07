cfc6b8cbfc418f5c36fec43b6f2d2d9102461be0
Erik Gilling <konkers@google.com>
2010-04-07 13:21:49 -0700

    sq

--- util.h
+++ util.h
@@ -1,6 +1,5 @@
 /*
  * Copyright 2010 Erik Gilling
- * Portions copyright 2007 James P Lynch
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
